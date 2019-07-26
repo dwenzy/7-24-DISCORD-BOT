176f5da5f6870ab57fed3e218e22b53e31eadfc6
dwenzy <53336897+dwenzy@users.noreply.github.com>
2019-07-26 21:04:05 +0300

    Add files via upload

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('Akasya Discorduna Hoşgeldin Susak İsmini ve Yaşını Yetkililere Bildirerek Kayıt Olabilirsin !tag yazıp Tagımızı Alarak Kendine Aramızda Yer Edinebilirsin!');
+
+};
